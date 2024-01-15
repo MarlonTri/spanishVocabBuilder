5c163175b3c394f9036e69c344ba31e13cdbc58a
Marlon Trifunovic <marlontrifunovic@gmail.com>
2024-01-15 14:00:01 -0800

    Package setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="espy",
+    version="0.1",
+    description="Espy - Espanol vocabulary builder",
+    author="Mar Tri",
+    # author_email='',
+    platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    license="BSD",
+    url="https://github.com/MarlonTri/spanishVocabBuilder",
+    packages=find_packages(),
+)
